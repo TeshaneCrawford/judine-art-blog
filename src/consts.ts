dd8d991148cbb4d62682dc4563bd9de8f3c00419
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-09-23 13:59:38 -0500

    Improved SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Judine Fiddler';
+export const SITE_TITLE = 'Judine Fiddler - Artist and Photographer';
 export const SITE_DESCRIPTION = 'Welcome to my art journal!';
+
+export const SITE_HEADER = 'Judine Fiddler';
 
 export const SOCIAL_LINKS = [
     {
