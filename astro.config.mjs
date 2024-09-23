0f8f8b55fee7f9badda59d19b596ab184521faf5
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-09-22 22:25:12 -0500

    Add icon, font, motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,21 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	image: {
-		domains: ["res.cloudinary.com"],
-		service: passthroughImageService()
-	},
-	experimental: {
-		contentLayer: true,
-	}
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), icon({
+        include: {
+            uil: ["instagram-alt", "twitter-alt", "facebook"],
+          },
+    })],
+    image: {
+        domains: ["res.cloudinary.com"],
+        service: passthroughImageService()
+    },
+    experimental: {
+        contentLayer: true,
+    }
 });
