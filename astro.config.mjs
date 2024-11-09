82683a67530b79018edaa204fc042684f7150d46
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-09 16:15:06 -0500

    refactor: scaffold project with ESLint + Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,12 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
-import mdx from '@astrojs/mdx';
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://judinefiddler.com',
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: "viewport",
-    },
-    // @ts-ignore
-    integrations: [mdx(), sitemap(), icon({
-        include: {
-            uil: ["instagram-alt", "twitter-alt", "facebook"],
-          },
-    })],
-    image: {
-        domains: ["res.cloudinary.com"],
-        service: passthroughImageService()
-    },
-    experimental: {
-        contentLayer: true,
-    },
-    output: "static",
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                  api: 'modern-compiler', // or "modern", "legacy"
-                  importers: [
-                    // ...
-                  ],
-                },
-              },
-        }
-    }
+  integrations: [tailwind(), sitemap(), icon()]
 });
