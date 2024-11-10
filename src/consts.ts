23ba7c1b966e040d0b4b8788711f09f12e19300e
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-10 18:24:35 -0500

    Add global consts for website

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export const SITE_TITLE = 'Judine Fiddler - Artist and Photographer';
+export const SITE_DESCRIPTION = 'Welcome to my art journal!';
+
+export const SITE_HEADER = 'Judine Fiddler';
+
+export const SOCIAL_LINKS = [
+    {
+        instagram: 'https://www.instagram.com/judinefiddler/'
+    }
+]
