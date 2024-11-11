55134df532f296066e2b454f54b449947ad41805
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-11 14:38:15 -0500

    update page look

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import { imageService } from "@unpic/astro/service";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://judinefiddler.com',
 
-  integrations: [tailwind(), sitemap(), icon()],
+  integrations: [tailwind(), sitemap(), icon({
+    include: {
+      uil: ["instagram-alt", "twitter-alt", "facebook"],
+    },
+  })],
 
   image: {
     domains: ["res.cloudinary.com"],
@@ -19,5 +25,7 @@ export default defineConfig({
       placeholder: "blurhash",
     }),
 },
-output: "server",
+
+  output: "server",
+  adapter: vercel(),
 });
